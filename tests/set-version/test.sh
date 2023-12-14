786369f1741f0758282a666cbb76e58438ef787f
Jonas Sjödin <jonas.k.sjodin@gmail.com>
2023-12-14 22:19:55 +0100

    Adding a custom github action for setting a version

---
+++ tests/set-version/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+commitTags=$(git tag --points-at HEAD)
+if [ -z "$commitTags" ]; then
+  echo "ERROR: No tags exists on this commit."
+  exit 1
+fi
+
+found=false
+for tag in $commitTags; do
+  if [ "$tag" == "$VERSION" ]; then
+    found=true
+    break
+  fi
+done
+
+if [ "$found" == "false" ]; then
+  echo "ERROR: Did not find a matching tag. Expected to find $VERSION in '$commitTags'"
+  exit 1
+fi
+
+echo "Tests completed, success!"
